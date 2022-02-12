1ad79dfd277c5d04c935166c35bd197fce76d115
Barry <bhanubhattarai13@gmail.com>
2022-02-12 18:35:38 +0530

    Added FPS in the window title

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "VAO.h"
 #include "Shader.h"
 #include "EBO.h"
+#include <string>
 
 int main() {
     std::cout << "Sage v" << Sage_VERSION_MAJOR << std::endl;
@@ -14,10 +15,10 @@ int main() {
     window.makeCurrentContext();
     gladLoadGLLoader((GLADloadproc) glfwGetProcAddress);
     float vertices[] = {
-            -0.5f, 0.5, 0, 1, 0, 0,
-            -0.5f, -0.5, 0, 0, 1, 0,
+            -0.5f, 0.5, 0, 1, 00.5, 0,
+            -0.5f, -0.5, 0, 0, 1, 00.5,
             0.5f, -0.5, 0, 0, 0, 1,
-            0.5, 0.5, 0, 1, 1, 1
+            0.5, 0.5, 0, 1, 0.7, 1
 
     };
 
@@ -37,12 +38,24 @@ int main() {
 
     vao.linkAttrib(vbo, 0, 3, GL_FLOAT, 6 * sizeof(float), nullptr);
     vao.linkAttrib(vbo, 1, 3, GL_FLOAT, 6 * sizeof(float), (void *) (3 * sizeof(float)));
-
+    double currentTime = 0, previousTime = 0, timeDiff;
+    unsigned int fpsCounter = 0;
     glClearColor(0.5, 0.5, 0.5, 1);
+    glfwSwapInterval(1);
     while (!window.shouldClose()) {
+        currentTime = glfwGetTime();
+        timeDiff = currentTime - previousTime;
+        fpsCounter++;
+        if (timeDiff >= 0.5) {
+            std::string FPS = std::to_string((unsigned  int)( fpsCounter/timeDiff));
+            std::string newTitle = "Sage " + FPS + " FPS";
+            glfwSetWindowTitle(window.instance, newTitle.c_str());
+            previousTime = currentTime;
+            fpsCounter = 0;
+        }
         glClear(GL_COLOR_BUFFER_BIT);
         shader.use();
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
         window.swapBuffer();
         glfwPollEvents();
     }
